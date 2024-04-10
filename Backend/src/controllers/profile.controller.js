9c7a35977a4c5f9d5f078e054acdf3a2c8301fbe
manpreetpiploda <manpreetpiploda@gmail.com>
2024-04-10 11:08:00 +0530

    controllers completed

--- Backend/src/controllers/profile.controller.js
+++ Backend/src/controllers/profile.controller.js
@@ -3,7 +3,8 @@ import { User } from '../models/user.model.js';
 import mongoose from 'mongoose';
 import CourseProgress from '../models/courseProgress.model.js';
 import uploadImageToCloudinary from '../utils/imageUploader.js';
-import convertSecondsToDuration from '../utils/secToDuration.js'
+import convertSecondsToDuration from '../utils/secToDuration.js';
+import Course from '../models/course.model.js';
 
 
 const updateProfile = async (req, res) => {
@@ -143,26 +144,103 @@ const updateDisplayPicture = async(req, res) =>{
 }
 
 const getEnrolledCourses = async (req, res) =>{
-    try{
-        const id = req.user.id;
+    try {
+    const userId = req.user.id
+    let userDetails = await User.findOne({
+      _id: userId,
+    })
+      .populate({
+        path: "courses",
+        populate: {
+          path: "courseContent",
+          populate: {
+            path: "subSection",
+          },
+        },
+      })
+      .exec()
+    console.log(" user Detais are : " , userDetails);
+    userDetails = userDetails.toObject()
+    var SubsectionLength = 0
+    for (var i = 0; i < userDetails.courses.length; i++) {
+      let totalDurationInSeconds = 0
+      SubsectionLength = 0
+      for (var j = 0; j < userDetails.courses[i].courseContent.length; j++) {
+        totalDurationInSeconds += userDetails.courses[i].courseContent[j].
+        subSection.reduce((acc, curr) => acc + parseInt(curr.timeDuration), 0)
 
-        const user= await User.findById({_id: id});
-
-        const coursses=[];
-        for(const courseId of user){
-            coursses.push(courseId);
-        }
-
+        userDetails.courses[i].totalDuration = convertSecondsToDuration(totalDurationInSeconds)
+        SubsectionLength +=
+          userDetails.courses[i].courseContent[j].subSection.length
+      }
+      let courseProgressCount = await CourseProgress.findOne({
+        courseID: userDetails.courses[i]._id,
+        userId: userId,
+      })
+      courseProgressCount = courseProgressCount?.completedVideos.length
+      if (SubsectionLength === 0) {
+        userDetails.courses[i].progressPercentage = 100
+      } else {
+        // To make it up to 2 decimal point
+        const multiplier = Math.pow(10, 2)
+        userDetails.courses[i].progressPercentage =
+          Math.round(
+            (courseProgressCount / SubsectionLength) * 100 * multiplier
+          ) / multiplier
+      }
     }
-    catch(error){
 
+    if (!userDetails) {
+      return res.status(400).json({
+        success: false,
+        message: `Could not find user with id: ${userDetails}`,
+      })
     }
+    return res.status(200).json({
+      success: true,
+      data: userDetails.courses,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
 }
 
+const instructorDashboard = async (req, res) => {
+  try {
+    const courseDetails = await Course.find({ instructor: req.user.id })
+
+    const courseData = courseDetails.map((course) => {
+      const totalStudentsEnrolled = course.studentsEnrolled.length
+      const totalAmountGenerated = totalStudentsEnrolled * course.price
+
+      // Create a new object with the additional fields
+      const courseDataWithStats = {
+        _id: course._id,
+        courseName: course.courseName,
+        courseDescription: course.courseDescription,
+        // Include other course properties as needed
+        totalStudentsEnrolled,
+        totalAmountGenerated,
+      }
+
+      return courseDataWithStats
+    })
+
+    res.status(200).json({ courses: courseData })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Server Error" })
+  }
+}
 
 export {
     updateProfile,
     deleteAccount,
     getAllUserDetails,
     updateDisplayPicture,
+    getEnrolledCourses,
+    instructorDashboard
 }
---
+++ Backend/src/controllers/section.controller.js
@@ -0,0 +1,143 @@
+import Section  from '../models/section.model.js';
+import SubSection from '../models/subSection.model.js';
+import Course from '../models/course.model.js';
+
+const createSection = async (req, res) => {
+    try {
+		// Extract the required properties from the request body
+		const { sectionName, courseId } = req.body;
+
+		// Validate the input
+		if (!sectionName || !courseId) {
+			return res.status(400).json({
+				success: false,
+				message: "Missing required properties",
+			});
+		}
+
+		// Create a new section with the given name
+		const newSection = await Section.create({ sectionName });
+
+		// Add the new section to the course's content array
+		const updatedCourse = await Course.findByIdAndUpdate(
+			courseId,
+			{
+				$push: {
+					courseContent: newSection._id,
+				},
+			},
+			{ new: true }
+		)
+			.populate({
+				path: "courseContent",
+				populate: {
+					path: "subSection",
+				},
+			})
+			.exec();
+
+		// Return the updated course object in the response
+		res.status(200).json({
+			success: true,
+			message: "Section created successfully",
+			updatedCourse,
+		});
+	} catch (error) {
+		// Handle errors
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+			error: error.message,
+		});
+	}
+
+}
+
+const updateSection = async (req, res) => {
+    try {
+		const { sectionName, sectionId,courseId } = req.body;
+		const section = await Section.findByIdAndUpdate(
+			sectionId,
+			{ sectionName },
+			{ new: true }
+		);
+
+		const course = await Course.findById(courseId)
+		.populate({
+			path:"courseContent",
+			populate:{
+				path:"subSection",
+			},
+		})
+		.exec();
+
+		res.status(200).json({
+			success: true,
+			message: section,
+			data:course,
+		});
+	} catch (error) {
+		console.error("Error updating section:", error);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+}
+
+const deleteSection = async ( req, res) => {
+    try {
+
+		const { sectionId, courseId }  = req.body;
+        const section = await Section.findById(sectionId);
+
+		if(!section) {
+			return res.status(404).json({
+				success:false,
+				message:"Section not Found",
+			})
+		}
+        
+		await Course.findByIdAndUpdate(courseId, {
+			$pull: {
+				courseContent: sectionId,
+			}
+		})
+		
+		console.log(sectionId, courseId);
+		
+
+		//delete sub section
+		await SubSection.deleteMany({_id: {$in: section.subSection}});
+
+		await Section.findByIdAndDelete(sectionId);
+
+		//find the updated course and return 
+		const course = await Course.findById(courseId).populate({
+			path:"courseContent",
+			populate: {
+				path: "subSection"
+			}
+		})
+		.exec();
+
+		res.status(200).json({
+			success:true,
+			message:"Section deleted",
+			data:course
+		});
+	} catch (error) {
+		console.error("Error deleting section:", error);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+}
+
+
+export {
+    createSection,
+    updateSection,
+    deleteSection,
+}
